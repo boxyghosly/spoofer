21beecf72a5c425fa8bfa1fcb39f99e0dbf5e842
MIXBREAKER <banyu.ghosly@gmail.com>
2026-08-17 12:22:05 +0700

    Update spoofer.lua

--- spoofer.lua
+++ spoofer.lua
@@ -93,7 +93,18 @@ end
 Char()
 
 Players:FindFirstChild(decodedData.name).CharacterAdded:Connect(function(char)
-    Char()
+    task.spawn(function()
+        for i = 1, 10 do
+            task.wait(0.2)
+
+            if Players:FindFirstChild(decodedData.name)
+                and Players[decodedData.name].Character == char then
+
+                Char()
+                break
+            end
+        end
+    end)
 end)
 
 -- changes premium/verified status --
